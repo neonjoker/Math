07174fbc4b4815cd3920e14822213de3f2c64000
Neonjoker <wuzunhe@gmail.com>
2018-09-12 17:52:58 +0800

    Define GS/J algorithm

--- GS J algorithm.py
+++ GS J algorithm.py
@@ -5,3 +5,11 @@ def Jacobi_Method(A,b,Stop_Method):
     n = b.ndim
     x = np.array([0 for i in range(n)])
     if(Stop_Method == 0):
+        def r(x):
+            return np.linalg.norm((np.dot(A,x) - b),ord=2)
+    elif(Stop_Method == 1):
+        def r(x1,x2):
+            return np.linalg.norm((x1-x2),ord=2)
+    elif(Stop_Method == 2):
+        def r(x1,x2):
+            return np.linalg.norm((np.dot(A,x)))
