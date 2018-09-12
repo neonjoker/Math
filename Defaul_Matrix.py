e8986ecc3c07882fd3c4f894503f889f862c3247
Neonjoker <wuzunhe@gmail.com>
2018-09-12 17:11:47 +0800

    Define Default Matrix of this Project

---
+++ Defaul_Matrix.py
@@ -0,0 +1,21 @@
+import numpy as np
+def T_matrix(n):
+    t = np.zeros(n)
+    for(i in range(n-1))
+        t[i,i] = 2
+        t[i,i+1] = -1
+        t[i+1,i] = -1
+    t[n,n] = 2
+    return t
+
+def I_matrix(n):
+    I = np.matrix(n*n)
+    for(i in range(n))
+        I[i,i] = 1
+    return I
+
+def Default_matrix(n):
+    T = T_matrix(n)
+    I = I_matrix(n)
+    return (np.kron(T,I) + np.kron(I,T))
+
