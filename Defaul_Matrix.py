f3d48e7ba1cd1bba0569047aefe4f063ce524359
Neonjoker <wuzunhe@gmail.com>
2018-09-12 19:35:18 +0800

    Repair some bugs

--- Defaul_Matrix.py
+++ Defaul_Matrix.py
@@ -1,15 +1,15 @@
 import numpy as np
 def T_matrix(n):
-    t = np.zeros(n)
+    t = np.zeros((n,n))
     for i in range(n-1):
         t[i,i] = 2
         t[i,i+1] = -1
         t[i+1,i] = -1
-    t[n,n] = 2
+    t[n-1,n-1] = 2
     return t
 
 def I_matrix(n):
-    I = np.matrix(n*n)
+    I = np.zeros((n,n))
     for i in range(n):
         I[i,i] = 1
     return I
@@ -18,3 +18,4 @@ def Default_matrix(n):
     T = T_matrix(n)
     I = I_matrix(n)
     return (np.kron(T,I) + np.kron(I,T))
+
