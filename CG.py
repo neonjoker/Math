305fbd7985c53b364fbcf9961e36c5edced09363
neonjoker <wuzunhe@gmail.com>
2018-10-16 18:51:01 +0800

    Finish CG method

--- CG.py
+++ CG.py
@@ -1,105 +1,22 @@
 import numpy as np
-import Defaul_Matrix
 
-def iterator(A,b,x,t,p,e,Move_Forward,Stop_Method=0):
-    x = x.copy()
-    tmp_1 = x.copy()
-    Move_Forward(x,t,p)
-    if (Stop_Method != 2):
-        if (Stop_Method == 0):
-            def r():
-                rest = np.linalg.norm((np.dot(A, x) - b), ord=2)
-                return rest
-
-            def solve():
-                while (abs(r()) > e):
-                    Move_Forward(x,t,p)
-
-            solve()
-        elif (Stop_Method == 1):
-            def r(x1, x2):
-                return np.linalg.norm((x1 - x2), ord=2)
-
-            def solve():
-                nonlocal tmp_1
-                while (r(x, tmp_1) > e):
-                    tmp_1 = x.copy()
-                    Move_Forward(x,t,p)
-
-            solve()
-    elif (Stop_Method == 2):
-        def r(x1, x2, x3):
-            d1 = np.linalg.norm((x1 - x2), ord=2)
-            d2 = np.linalg.norm((x2 - x3), ord=2)
-            return (d2 * d2 / (d1 - d2))
-
-        def solve():
-            Move_Forward(x,t,p)
-            nonlocal tmp_1
-            tmp_2 = tmp_1.copy()
-            tmp_1 = x.copy()
-            while (r(x, tmp_1, tmp_2) < e):
-                Move_Forward(x,t,p)
-                tmp_2 = tmp_1.copy()
-                tmp_1 = x.copy()
-
-        solve()
+def CG(A,b,e,x):
+    r = np.dot(A,x) - b
+    p = -(np.copy(r))
+    while(np.linalg.norm((np.dot(A, x) - b), ord=2)>e):
+        q = np.dot(A,p)
+        a = np.dot(np.transpose(r),r)[0][0]/(np.dot(np.transpose(p),q)[0][0])
+        x = x + a * p
+        beta = 1 / (np.dot(np.transpose(r),r)[0][0])
+        r = r + a * q
+        beta = beta * np.dot(np.transpose(r),r)[0][0]
+        p = -r + beta * p
     return x
 
-def Gauss_Method(A,b,e=1e-6,Stop_Method=0):
-    n = b.size
-    x = np.zeros((n,1),dtype='float64')
-    def Move_Forward(x):
-        for i in range(n):
-            x[i][0] = b[i][0]
-            for j in range(n):
-                if (j != i):
-                    x[i][0] = x[i][0] - A[i, j] * x[j][0]
-            x[i][0] = x[i][0] / A[i, i]
-        x = x.copy()
-    res = iterator(A,b,x,e,Move_Forward,Stop_Method)
-    return res
+import Defaul_Matrix
 
-def CG(A,b,e=1e-6,Stop_Method=0):
-    n = b.size
-    x = np.zeros(n,dtype='float64')
-    r = np.dot(A, b) - b
-    p = np.copy(r)
-    def Move_Forward(x,r,p):
-        q = np.dot(A,p)
-        a = -np.dot(np.transpose(r),p)/np.dot(np.transpose(p),q)[0][0]
-        for i in range(n):
-            x[i] = x[i] + a * p[i][0]
-        for i in range(n):
-            r[i][0] + a * q[i][0]
-        beta = np.dot(np.transpose(r),q)[0][0]/np.dot(np.transpose(p),q)[0][0]
-        for i in range(n):
-            p[i][0] = -r[i][0] + beta * p[i][0]
-    res = iterator(A, b, x, r, p, e, Move_Forward, Stop_Method)
-    return res
-
-
-def CG(A,b,e=1e-6,Stop_Method=0):
-    n = b.size
-    x = np.zeros(n,dtype='float64')
-    r = np.dot(A, b) - b
-    p = np.copy(r)
-    def Move_Forward(x):
-        nonlocal r,p
-        q = np.dot(A,p)
-        a = -np.dot(np.transpose(r),p)/np.dot(np.transpose(p),q)[0][0]
-        for i in range(n):
-            x[i] = x[i] + a * p[i][0]
-        for i in range(n):
-            r[i][0] + a * q[i][0]
-        beta = np.dot(np.transpose(r),q)[0][0]/np.dot(np.transpose(p),q)[0][0]
-        for i in range(n):
-            p[i][0] = -r[i][0] + beta * p[i][0]
-    res = iterator(A, b, x, r, p , e, Move_Forward, Stop_Method)
-    return res
-
-from Defaul_Matrix import Default_matrix
-A = Default_matrix(3)
-b = np.dot(A,np.array([[1] for i in range(9)]))
-res = CG(A,b)
-print(res)
+A = Defaul_Matrix.Default_matrix(3)
+b = np.dot(A,np.array([[1] for i in range(9)],dtype='float64'))
+x = np.array([[0] for i in range(9)],dtype='float64')
+Res = CG(A,b,1e-6,x)
+print(Res)
