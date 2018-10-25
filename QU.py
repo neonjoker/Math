9fbceed1dd8fb9e9ca8b4b6cf91b5f330077cffb
neonjoker <wuzunhe@gmail.com>
2018-10-25 17:43:43 +0800

    Finish CGS/MGS method!

--- QU.py
+++ QU.py
@@ -52,7 +52,7 @@ def Householder(A):
         a[0] = a[0] - alpha
         return (alpha,b)
     def multiply_householder(a,b,u):
-        a =  a - np.dot(np.transpose(u),a) / b * u
+        return  a - np.dot(np.transpose(u),a) / b * u
     for i in range(s):
         res = householder(A[i:m:,i])
         d[i] = res[0]
@@ -62,8 +62,64 @@ def Householder(A):
     R = np.triu(A)
     for i in range(s):
         R[i,i] = d[i]
-    Q = np.ones((m,m),dtype='float64')
+    Q = np.diag(np.diag(np.ones((m,m),dtype='float64')))
     for i in range(s):
         for j in range(i,m):
-            multiply_householder(Q[i:m:,j],b[i],A[i:m:,i])
+            Q[i:m:,j] = multiply_householder(Q[i:m:,j],b[i],A[i:m:,i])
     return (Q,R)
+
+def Givens(A):
+    n = A.shape[1]
+    m = A.shape[0]
+    s = min(n, m) - 1
+    d = np.zeros(s)
+    b = np.zeros(s)
+    Q = np.diag(np.diag(np.ones((m,m),dtype = 'float64')))
+
+    def givens(i,j,a):
+        if(abs(a[j])<1e-10):
+            c = 1
+            s = 0
+        if(abs(a[j])>=abs(a[i])):
+            t = a[i] / a[j]
+            s = 1 / np.sqrt(1 + t * t)
+            c = s * t
+        else:
+            t = a[j] / a[i]
+            c = 1 / np.sqrt(1 + t * t)
+            s = c * t
+        a[i] = np.sqrt(a[i]*a[i] + a[j]*a[j])
+        if(abs(c)<1e-10):
+            return 1
+        elif(abs(s)<abs(c)):
+            return sgn(c) * s / 2
+        else:
+            return 2 * sgn(s) / c
+
+    for j in range(s):
+        for i in range(j,n):
+            A[i,j] = givens(i,j,A[:,j])
+            if(abs(A[i,j]-1)<1e-10):
+                c = 0
+                s = 1
+            elif(abs(A[i,j])<1):
+                s = 2 * A[i,j]
+                c = np.sqrt(1 - s * s)
+            else:
+                c = A[i,j] / 2
+                s = np.sqrt(1 - c * c)
+            q1 = Q[i,j]
+            q2 = Q[j,j]
+            Q[i,j] = c * q1 - s * q2
+            Q[j,j] = s * q1 + c * q2
+            for k in range(j+1,n):
+                a = A[i,k]
+                b = A[j,k]
+                q1 = Q[i,k]
+                q2 = Q[j,k]
+                A[i,k] = c * a + s * b
+                A[j,k] = c * b - s * a
+                Q[i,k] = c * q1 - s * q2
+                Q[j,k] = s * q1 + c * q2
+    return (Q,A)
+
