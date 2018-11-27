9f8a7bdff61b902be6494ee56e5c0b895ce2a473
neonjoker <wuzunhe@gmail.com>
2018-11-27 22:26:36 +0800

    Finish Basic Eig Algorithm.

--- Eig.py
+++ Eig.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from QU import MGS
 sgn = lambda x: 1 if x > 0 else -1 if x < 0 else 0
 
@@ -9,84 +11,152 @@ def EigOfTmatrix(T):
         l[i] = 2 * (1 - np.cos((i+1) * np.pi / (n+1)))
     return l
 
-def Power_Method(A,u,e,m):
-    for k in range(m):
+def Power_Method(A,u,e,m,*out):
+    #out参数第一个为主特征值
+    if(out):
+        eigvals = np.array([],dtype='float64')
+        for k in range(m):
+            b = max(abs(u))
+            u = u / b
+            v = np.dot(A, u)
+            b = max(abs(v))
+            eigvals = np.append(eigvals,out[0]-b)
+            if (b == 0):
+                raise ValueError('A has eigenvalue 0,select new initial vector and restart')
+            w = v / b
+            if (abs(out[0]-b) < e):
+                data = {'Eigvalue':eigvals,'IterateTimes':np.arange(1,k+2)}
+                df = pd.DataFrame(data)
+                return (b, w, df)
+            else:
+                u = w
+        data = {'Eigvalue': eigvals, 'IterateTimes': np.arange(1, m + 1)}
+        df = pd.DataFrame(data)
+        print('failed!')
+        return (b, w, df)
+    else:
+        for k in range(m):
+            b = max(abs(u))
+            u = u / b
+            v = np.dot(A, u)
+            b = max(abs(v))
+            if (b == 0):
+                raise ValueError('A has eigenvalue 0,select new initial vector and restart')
+            w = v / b
+            if (np.max(u - w) < e):
+                return (b, w)
+            else:
+                u = w
+        raise ValueError('Maximum number of iteration exceeded')
+
+
+def Power_Method_Aitken(A,u,e,m,*out):
+    if(out):
+        eigvals = np.array([], dtype='float64')
         b = max(abs(u))
+        b_0 = 0.0
+        b_1 = 0.0
         u = u / b
-        v = np.dot(A,u)
-        b = max(v)
-        if(b == 0):
-            raise ValueError('A has eigenvalue 0,select new initial vector and restart')
-        w = v / b
-        if(np.max(u-w)<e):
-            return (b,w)
-        else:
-            u = w
-    raise ValueError('Maximum number of iteration exceeded')
+        for k in range(m):
+            v = np.dot(A, u)
+            b = max(abs(v))
+            b_ = b_0 - (b_1 - b_0) * (b_1 - b_0) / (b - 2 * b_1 + b_0)
+            eigvals = np.append(eigvals, out[0] - b_)
+            if (b == 0):
+                raise ValueError('A has eigenvalue 0,select new initial vector and restart')
+            if (k == 0):
+                w = v / b
+            else:
+                w = v / b
+            if (np.linalg.norm(abs(out[0]-b) < e)):
+                data = {'Eigvalue': eigvals, 'IterateTimes': np.arange(1, k + 2)}
+                df = pd.DataFrame(data)
+                return (b_, w , df)
+            else:
+                u = w
+                b_0 = b_1
+                b_1 = b
+        data = {'Eigvalue': eigvals, 'IterateTimes': np.arange(1, m + 1)}
+        df = pd.DataFrame(data)
+        print('failed!')
+        return (b_, w, df)
+    else:
+        b = max(abs(u))
+        b_0 = 0.0
+        b_1 = 0.0
+        u = u / b
+        for k in range(m):
+            v = np.dot(A, u)
+            b = max(abs(v))
+            b_ = b_0 - (b_1 - b_0) * (b_1 - b_0) / (b - 2 * b_1 + b_0)
+            if (b == 0):
+                raise ValueError('A has eigenvalue 0,select new initial vector and restart')
+            if (k == 0):
+                w = v / b
+            else:
+                w = v / b
+            if (np.linalg.norm(u - w) < e):
+                return (b_, w)
+            else:
+                u = w
+                b_0 = b_1
+                b_1 = b
+        print('failed!')
 
-'''def Power_Method_Aitken(A,u,e,m):
-    b = max(u)
-    b_1 = 0.0
-    b_0 = 0.0
-    u = u / b
-    for k in range(m):
-        v = np.dot(A,u)
-        b = max(v)
-        b_ = b_0 - (b_1 - b_0)**2 / (b - 2 * b_1 + b_0)
-        if (b == 0):
-            raise ValueError('A has eigenvalue 0,select new initial vector and restart')
-        if(k != 0):
-            w = v / b_
-        elif(k == 0):
-            w = v / b
-        if (np.max(u - w) < e):
-            return (b_, w)
-        else:
-            u = w
-            b_0 = b_1
-            b_1 = b_
-'''
 
-def Power_Method_Aitken(A,u,e,m):
-    b = max(abs(u))
-    b_0 = 0.0
-    b_1 = 0.0
-    u = u / b
-    for k in range(m):
-        v = np.dot(A,u)
-        b = max(v)
-        b_ = b_0 - (b_1 - b_0) * (b_1 - b_0) / (b - 2 * b_1 + b_0)
-        if (b == 0):
-            raise ValueError('A has eigenvalue 0,select new initial vector and restart')
-        if(k==0):
-            w = v / b
-        else:
-            w = v / b
-        if(np.linalg.norm(u-w)<e):
-            return (b_,w)
-        else:
-            u = w
-            b_0 = b_1
-            b_1 = b
+def Power_Method_Rayleigh(A,u,e,m,*out):
+    if(out):
+        eigvals = np.array([], dtype='float64')
+        for k in range(m):
+            r = np.dot(np.dot(np.transpose(u), A), u) / np.linalg.norm(u, ord=2)
+            u = u / r
+            v = np.dot(A, u)
+            r = np.dot(np.dot(np.transpose(v), A), v) / np.linalg.norm(v, ord=2)
+            eigvals = np.append(eigvals, out[0] - r)
+            if (r == 0):
+                raise ValueError('A has eigenvalue 0,select new initial vector and restart')
+            w = v / r
+            if (np.max(abs(out[0]-r)) < e):
+                data = {'Eigvalue': eigvals, 'IterateTimes': np.arange(1, k + 2)}
+                df = pd.DataFrame(data)
+                return (r, w, df)
+            else:
+                u = w
+        data = {'Eigvalue': eigvals, 'IterateTimes': np.arange(1, m + 1)}
+        df = pd.DataFrame(data)
+        print('failed!')
+        return (r, w, df)
+    else:
+        for k in range(m):
+            r = np.dot(np.dot(np.transpose(u), A), u) / np.linalg.norm(u, ord=2)
+            u = u / r
+            v = np.dot(A, u)
+            r = np.dot(np.dot(np.transpose(v), A), v) / np.linalg.norm(v, ord=2)
+            if (r == 0):
+                raise ValueError('A has eigenvalue 0,select new initial vector and restart')
+            w = v / r
+            if (np.max(abs(u - w)) < e):
+                return (r, w)
+            else:
+                u = w
+        raise ValueError('Maximum number of iteration exceeded')
 
-def Power_Method_Rayleigh(A,u,e,m):
-    for k in range(m):
-        r = np.dot(np.dot(np.transpose(u), A), u) / np.linalg.norm(u, ord=2)
-        u = u / r
-        v = np.dot(A,u)
-        r = np.dot(np.dot(np.transpose(v),A),v) / np.linalg.norm(v,ord = 2)
-        if(r == 0):
-            raise ValueError('A has eigenvalue 0,select new initial vector and restart')
-        w = v / r
-        if(np.max(u-w)<e):
-            return (r,w)
-        else:
-            u = w
-    raise ValueError('Maximum number of iteration exceeded')
-
-def Wieldant(A,d,u,v,e,m):
-    A = A - d * np.outer(v,v)
-    return Power_Method(A,u,e,m)
+def Wieldant(A,u,e,m):
+    (l,x) = Power_Method(A,u,e,m)
+    t = np.linalg.norm(x)
+    u = x.copy()
+    u[0] = u[0] - t
+    b = 1 / (t * (t - x[0]))
+    S = np.identity(A.shape[0]) - b * np.outer(u,u)
+    B = np.dot(np.dot(S,A),S.T)
+    '''
+    B = A - l * np.dot(x,np.transpose(x))
+    '''
+    '''
+    p = np.dot(A,u) * b
+    v = p - (1/2) * (b * np.dot(u,p)) * u
+    B = A - (np.dot(u,np.transpose(v)) + np.dot(v,np.transpose(u)))'''
+    return Power_Method_Aitken(B[1:,1:],u[1:],e,m)
 
 def inv_Power_Method(A,u,q,e,m):
     for k in range(m):
@@ -243,20 +313,24 @@ def binary_eig(A,e,start,end):
 if __name__ == '__main__':
     from Defaul_Matrix import T_matrix
     A = T_matrix(100)
-    eig = np.linalg.eig(A)
-    l = max(eig[0])
-    v = eig[1][np.argmax(eig[0])]
-    #res = Power_Method_Rayleigh(A,np.ones(100),1e-10,10000)
-    #res = Wieldant(A,l,np.ones(100),v,1e-10,10000)
+    eig = EigOfTmatrix(A)
+    l_1 = max(eig)
+    #res = Power_Method_Rayleigh(A,np.ones(101),1e-10,50000,l_1)
+    res = Wieldant(A,np.ones(100),1e-10,50000)
     #res = simultaneous_iteration(A,1e-10)
     #res = Limit_Loop_Jacobi(A,100,1e-10,50000)
     #res = binary_eig(A,1e-10,1,2)
     #print(res)
+    #df = res[2]
+    #plt.rcParams['font.sans-serif'] = ['SimHei']
+    #plt.rcParams['axes.unicode_minus'] = False
+    #df.plot(x = 'IterateTimes',y = 'Eigvalue',style = '-o')
+    #plt.show()
     #res = inv_Power_Method(A,np.ones(100),res,1e-10,5)
-    #print(res)
+    print(res)
     #print(max(res))
-    Eig = EigOfTmatrix(A)
-    eig = np.transpose(eig[0])
-    eig.sort()
-    Eig.sort()
-    print(eig - Eig)
+    #Eig = EigOfTmatrix(A)
+    #eig = np.transpose(eig[0])
+    #eig.sort()
+    #Eig.sort()
+    #print(eig - Eig)
